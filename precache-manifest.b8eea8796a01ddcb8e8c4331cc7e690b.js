10126839dd597dbe912b92a070c81abd4e319ea1
TheNinza <niks.a3198@gmail.com>
2020-06-09 19:21:42 +0530

    Updates

---
+++ precache-manifest.b8eea8796a01ddcb8e8c4331cc7e690b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6de7f785d6571ec892b1101d560aff43",
+    "url": "/monsters-roledex/index.html"
+  },
+  {
+    "revision": "9cfdaae04dfb0df1f90a",
+    "url": "/monsters-roledex/static/css/main.27edc8f9.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-roledex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-roledex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cfdaae04dfb0df1f90a",
+    "url": "/monsters-roledex/static/js/main.369cedd3.chunk.js"
+  },
+  {
+    "revision": "0e9bc445ccb1118a3b9d",
+    "url": "/monsters-roledex/static/js/runtime-main.632dcb50.js"
+  }
+]);
